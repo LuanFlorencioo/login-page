1b328b22acd151d35c5d881f222add240549daf8
L7AN <luanflorencio87@gmail.com>
2021-03-22 13:31:35 -0300

    version 1.0

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-function signup() {
-    let email = document.querySelector('input#email-txt');
-    let password = document.querySelector('input#password-txt');
+let email = document.querySelector('input#email-txt');
+let password = document.querySelector('input#password-txt');
+let invalid = document.querySelector('.login')
+let mensageInvalid = document.createElement('p')
+invalid.appendChild(mensageInvalid);
 
-    if (email.value === 'test' && password.value === '6903') {
-        alert('Done!');
+function signup() {
+    
+    if (email.value === 'client@user.com' && password.value === '6903') {
+        email.style.borderColor = '';
+        password.style.borderColor = '';
+        mensageInvalid.innerHTML = 'Done';
+        mensageInvalid.style.color = '#28db46'
     }
     else {
         email.style.borderColor = 'red';
         password.style.borderColor = 'red';
+        mensageInvalid.innerHTML = 'e-mail or password invalid!'
+        mensageInvalid.style.color = 'red'
     }
 }
